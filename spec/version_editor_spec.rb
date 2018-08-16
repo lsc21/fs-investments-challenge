ad7055a9e3068a25386eb3d80d5076a8df39e8c4
Louis Clotman <louis@clotman.name>
2018-08-15 21:04:13 -0400

    write more tests and add contexts

--- spec/version_editor_spec.rb
+++ spec/version_editor_spec.rb
@@ -8,30 +8,51 @@ describe VersionEditor do
     File.delete('outfile.yaml') if File.exists?('outfile.yaml')
   end
 
-  it 'bumps all the semver values in the file by a major version' do
-    test_bump level: 'major',
-      advisorUi: 'v3.0.0',
-      permissionsService: 'v2.0.0',
-      adminApi: 'v2.0.0'
+  context 'when editing the entire file' do
+
+    it 'bumps all the semver values by a major version' do
+      test_bump level: 'major',
+        advisorUi: 'v3.0.0',
+        permissionsService: 'v2.0.0',
+        adminApi: 'v2.0.0'
+    end
+
+    it 'bumps all the semver values by a minor version' do
+      test_bump level: 'minor',
+        advisorUi: 'v2.14.0',
+        permissionsService: 'v1.27.0',
+        adminApi: 'v1.32.0'
+    end
+
+    it 'bumps all the semver values by a patch version' do
+      test_bump level: 'patch',
+        advisorUi: 'v2.13.2',
+        permissionsService: 'v1.26.4',
+        adminApi: 'v1.31.1'
+    end
+
   end
 
-  it 'bumps all the semver values in the file by a minor version' do
-    test_bump level: 'minor',
-      advisorUi: 'v2.14.0',
-      permissionsService: 'v1.27.0',
-      adminApi: 'v1.32.0'
-  end
+  context 'when varying the output file location' do
 
-  it 'bumps all the semver values in the file by a patch version' do
-    test_bump level: 'patch',
-      advisorUi: 'v2.13.2',
-      permissionsService: 'v1.26.4',
-      adminApi: 'v1.31.1'
+    it 'writes to the default location' do
+      args = ["bump", "-f", "./artifacts/example.yaml", "-l", 'major']
+      VersionEditor.start(args)
+      expect(File.exists?('alternate.yaml')).to be_falsey
+      expect(File.exists?('outfile.yaml')).to be_truthy
+    end
+
+    it 'writes to an alternate location' do
+      args = ["bump", "-f", "./artifacts/example.yaml", "-l", 'major', '-o', 'alternate.yaml']
+      VersionEditor.start(args)
+      expect(File.exists?('alternate.yaml')).to be_truthy
+      expect(File.exists?('outfile.yaml')).to be_falsey
+    end
   end
 
   private
 
-  def test_bump level: level, advisorUi: advisorUi, permissionsService: permissionsService, adminApi: adminApi
+  def test_bump level:, advisorUi:, permissionsService:, adminApi:
     args = ["bump", "-f", "./artifacts/example.yaml", "-l", level]
     VersionEditor.start(args)
     yaml = YAML.load_file "outfile.yaml"
