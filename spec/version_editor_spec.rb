306ba75aedda834bb196bdfb81ef8447a966a346
Louis Clotman <louis@clotman.name>
2018-08-15 21:43:39 -0400

    more tests

--- spec/version_editor_spec.rb
+++ spec/version_editor_spec.rb
@@ -30,6 +30,16 @@ describe VersionEditor do
 
   context 'when targeting specific keys' do
 
+    it 'bumps all the semver values by a major version' do
+      test_bump options: [ "bump", "-f", "./artifacts/example.yaml", "-l", 'major', "-k", "testSemver2" ],
+        expectations: { advisorUi: 'v2.13.1', permissionsService: 'v1.26.3', adminApi: 'v1.31.0', testSemver2: "v4.0.0" }
+    end
+
+    it 'bumps all the semver values by a major version' do
+      test_bump options: [ "bump", "-f", "./artifacts/example.yaml", "-l", 'major', "-k", "dockerTag" ],
+        expectations: { advisorUi: 'v3.0.0', permissionsService: 'v2.0.0', adminApi: 'v2.0.0', testSemver2: "v3.2.3" }
+    end
+
   end
 
 
@@ -60,9 +70,10 @@ describe VersionEditor do
   def test_bump options:, expectations:
     VersionEditor.start(options)
     yaml = YAML.load_file "outfile.yaml"
-    expect(yaml['advisorUi']['image']['dockerTag']).to eq(expectations[:advisorUi])
-    expect(yaml['permissionsService']['image']['dockerTag']).to eq(expectations[:permissionsService])
-    expect(yaml['adminApi']['image']['dockerTag']).to eq(expectations[:adminApi])
+    expect(yaml['advisorUi']['image']['dockerTag']).to eq(expectations[:advisorUi]) if expectations.include?(:advisorUi)
+    expect(yaml['permissionsService']['image']['dockerTag']).to eq(expectations[:permissionsService]) if expectations.include?(:permissionsService)
+    expect(yaml['adminApi']['image']['dockerTag']).to eq(expectations[:adminApi]) if expectations.include?(:adminApi)
+    expect(yaml['permissionsService']['apiVersion']['testSemver2']).to eq(expectations[:testSemver2]) if expectations.include?(:testSemver2)
   end
 end
 
